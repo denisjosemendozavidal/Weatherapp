bc04472cb7bc6a478be906ae7747c58e881a990b
denisjosemendozavidal <denismendozavidal@gmail.com>
2022-09-23 15:33:39 -0500

    Updating

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,112 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import './App.css'
 import CardWeather from './Components/CardWeather'
-import iconImage from './Components/CardWeather'
 
 function App() {
-  
+
+  /*Inicial states for API info and getting location*/ 
+  const [Info, setInfo] = useState()
+  const [location, setLocation] = useState()
+
   
 
+
+/*useEffect to get the location*/ 
+
+useEffect (() => {
+    const success = pos => {
+    const obj = {
+        lat : pos.coords.latitude,
+        lon : pos.coords.longitude
+    }
+    setLocation (obj) 
+    }
+    navigator.geolocation.getCurrentPosition(success) 
+}, [])
+
+
+/*useEffect to get the info from the API*/ 
+
+useEffect (() => {
+  if (location) {
+    const myapikey = `7b698be720304d9cf7da0c7639e49b4b`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${myapikey}`;
+    axios.get(URL)
+        .then (res => setInfo(res.data))
+        .catch (err => console.log(err))
+
+  }
+}, [location])
+
+const tempC = Info?.main.temp - 273.15;
+const tempF = (tempC * 9/5) + 32; 
+const F = "°F";
+const C = "°C";
+
+/*Rendering the temp*/ 
+
+const [istrue, setIstrue] = useState();
+const [temp, setTemp] = useState("Click bellow to see the temperature");
+const [letter, setLetter] = useState("");
+
+const changetemp = () => {
+    
+  setIstrue(!istrue)
+
+  if (istrue) {
+    setTemp (tempC.toFixed(2))
+    setLetter (C)
+  } else {
+    setTemp (tempF.toFixed(2))
+    setLetter(F)
+  }
+}
+
+/*Getting the weather image to show*/ 
+
+
+const [iconImage, setIconImage] = useState("");
+const [brokenclouds, setBrokenclouds] = useState("./images/broken-clouds.jpg");
+const [overcastclouds, setOvercastclouds] = useState("./images/overcast-clouds.jpg")
+const [fewclouds, setFewclouds] = useState("./images/few-clouds.jpg")
+const [clearsky, setClearsky] = useState("./images/Clear-Sky.jpg")
+
+
+useEffect(() => {
+      if (Info?.weather[0].description === "broken clouds") {
+        setIconImage(brokenclouds)
+      } else if (Info?.weather[0].description === "overcast clouds") {
+        setIconImage(overcastclouds)
+      } else if (Info?.weather[0].description === "few clouds") {
+        setIconImage(fewclouds)
+      } else if (Info?.weather[0].description === "Clear Sky") {
+        setIconImage(clearsky)
+      }
+
+},[Info])
+
+/*Declaring for props*/ 
+const humidity = Info?.main.humidity;
+const typeofweather = Info?.weather[0].description;
+const city = Info?.name;
+const country = Info?.sys.country;
+
+  
+  console.log(iconImage); //This is to validate that the info is coming.
+
   return (
-    <div className="App">
-      <CardWeather/>
+    <div className="App" style={{backgroundImage: `url(${iconImage})`}}>
+      <CardWeather
+        info = {Info}
+        iconImage = {iconImage}
+        temp = {temp}
+        letter = {letter}
+        changetemp = {changetemp}
+        humidity = {humidity}
+        typeofweather = {typeofweather}
+        city = {city}
+        country = {country}
+      />
     </div>
   )
 }
--- src/Components/CardWeather.jsx
+++ src/Components/CardWeather.jsx
@@ -5,89 +5,20 @@ import  { useEffect, useState, React } from 'react'
 
 
 
-const CardWeather = () => {
-
-/*Inicial states for API info and getting location*/ 
-  const [Info, setInfo] = useState()
-  const [location, setLocation] = useState()
-
-/*Getting the weather image to show*/ 
-
-  const description = Info?.weather[0].description;
-  const [iconImage, setIconImage] = useState("");
-
-  useEffect(() => {
-    const brokenclouds = "./images/broken-clouds.jpg";
-    const overcastclouds= "./images/overcast-clouds.jpg";
-    if (description === "broken clouds") {
-      setIconImage(brokenclouds)
-    } else if (description === "overcast clouds") {
-      setIconImage(overcastclouds)
-    }
-  },[])
-
-/*useEffect to get the location*/ 
-
-useEffect (() => {
-    const success = pos => {
-    const obj = {
-        lat : pos.coords.latitude,
-        lon : pos.coords.longitude
-    }
-    setLocation (obj) 
-    }
-    navigator.geolocation.getCurrentPosition(success) 
-}, [])
+const CardWeather = ({Info, iconImage, temp, letter, changetemp, humidity, typeofweather, city, country}) => {
 
 
-/*useEffect to get the info from the API*/ 
-
-useEffect (() => {
-    const myapikey = `7b698be720304d9cf7da0c7639e49b4b`;
-    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${location?.lat}&lon=${location?.lon}&appid=${myapikey}`;
-    axios.get(URL)
-        .then (res => setInfo(res.data))
-        .catch (err => console.log(err))
-}, [location])
-
-const tempC = Info?.main.temp - 273.15;
-const tempF = (tempC * 9/5) + 32; 
-const F = "°F";
-const C = "°C";
-
-/*Rendering the temp*/ 
-
-const [istrue, setIstrue] = useState();
-const [temp, setTemp] = useState("Click bellow to see the temperature");
-const [letter, setLetter] = useState("");
-
-const changetemp = () => {
-    
-  setIstrue(!istrue)
-
-  if (istrue) {
-    setTemp (tempC.toFixed(2))
-    setLetter (C)
-  } else {
-    setTemp (tempF.toFixed(2))
-    setLetter(F)
-  }
-}
-
-
-
-console.log(description); //This is to validate that the info is coming.
 
 
   return (
     <article className='card'>
         <h1>Weather app</h1>
-        <h4 className='city'>{`${Info?.name}, ${Info?.sys.country}`}</h4>
+        <h4 className='city'>{`${city}, ${country}`}</h4>
         <div className='iconandinfo'>
           <img className='icon' src={`${iconImage}`} alt="Loading..." /> 
           <ul className='info'>
-            <li className='typeofweather'>{`${description}`}</li>
-            <li className='humidity'><span>Humidity:</span> {`${Info?.main.humidity}%`}</li>
+            <li className='typeofweather'>{`${typeofweather}`}</li>
+            <li className='humidity'><span>Humidity:</span> {`${humidity}%`}</li>
             <li className='temp'><span>Temperature:</span>{`${temp}${letter}`}</li>
           </ul>
         </div>
